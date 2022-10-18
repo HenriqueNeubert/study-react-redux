dfbf2d556aac106b59458b441b7346f1683f36b7
HenriqueNeubert <neubert.study@gmail.com>
2022-10-18 17:51:28 -0300

    feat: working with component

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,12 +1,15 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import './index.css'; //? style
+import ReactDOM from 'react-dom'; //? método
+import React from 'react'; //? react
+
+import Primeiro from './components/basicos/Primeiro';//? component
 
 const el = document.getElementById('root');
 const tag = <strong>Olá React!!!</strong>
 
 ReactDOM.render(
   <div>
-    {tag}
+    <Primeiro></Primeiro>
   </div>, 
   el
 )
