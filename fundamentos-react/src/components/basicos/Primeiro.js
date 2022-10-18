67dbcfeed88471403950970d86372cb0658fac53
HenriqueNeubert <neubert.study@gmail.com>
2022-10-18 17:58:15 -0300

    feat: working with JSX, components and variables

--- fundamentos-react/src/components/basicos/Primeiro.js
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,4 +1,12 @@
+import React from 'react';
+
 export default function Primeiro ()
 {
-  return 'Primeiro Componente';
+  const msg = 'Primeiro paragrafo';
+  return (
+    <div>
+      <h2>Primeiro Componente</h2>
+      <p>{ msg }</p>
+    </div>
+  )
 }
