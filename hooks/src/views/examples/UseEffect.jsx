2dd928d6bb6b67d0669658245bbd728b37972b11
HenriqueNeubert <neubert.study@gmail.com>
2022-11-22 11:40:45 -0300

    feat: working with hooks-useEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFatorial(num) 
+{
+    const n = parseInt(num)
+    if(n < 0) return -1
+    if(n === 0) return 1
+    return calcFatorial(n - 1) * n
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1)
+    const[fatorial, setFatorial] = useState(1)
+    const[status, setStatus] = useState("Ímpar")
+
+    useEffect(function() {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    useEffect(function() { //efeito colateral
+        if(fatorial > 100) {
+            document.title = 'Eita!!!'
+        } else{
+            document.title = 'React App'
+        }
+    })
+
+    useEffect(function() {
+        setStatus(number % 2 === 0 ? "Par" : "Impar")
+    }, [number])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial: </span>
+                    <span className="text red">{ fatorial === -1 ? 'Não existe' : fatorial }</span>
+                </div>
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{ status }</span>
+                </div>
+                <input type="number" className="input" 
+                value={number} onChange={(e) => setNumber(e.target.value)}/>
+            </div>          
         </div>
     )
 }
