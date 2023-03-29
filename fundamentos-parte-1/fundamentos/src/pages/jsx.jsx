a11701ef32f0d434810609faac55222722795b2d
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-03-29 20:29:19 -0300

    feat: working with css

---
+++ fundamentos-parte-1/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,17 @@
+export default function Jsx() {
+  const a = 4
+  const b = 3
+  const titulo = "JSX-conceito"
+  const objeto = {nome: "Henrique", idade: 25}
+  function subtitulo(){
+    return <h2>Este é o subtitulo</h2>
+  }
+  return (
+    <div>
+      <h1>{titulo.toLocaleUpperCase()}</h1>
+      {subtitulo()}
+      {a * b}
+      <p>{JSON.stringify(objeto)}</p>
+    </div>
+  )
+}
