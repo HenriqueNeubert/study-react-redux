8903dddde9872d222479b5aff72102525d6433a8
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-03-29 21:10:28 -0300

    feat: first steps, jsx, components and props

---
+++ fundamentos-parte-1/fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../components/Cabecalho";
+
+export default function Exemplo() {
+  return (
+    <>
+      <h1>Exemplo</h1>
+      <Cabecalho titulo="Fundamentos de Next.js and React.js"></Cabecalho>
+      <Cabecalho></Cabecalho>
+    </>
+  )
+}
