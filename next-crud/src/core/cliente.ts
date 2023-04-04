4f8d9f45ee7a6489cc539266b0b4d98338f0695d
HenriqueNeubert <68157909+HenriqueNeubert@users.noreply.github.com>
2023-04-04 19:16:21 -0300

    feat: created core cliente.ts

---
+++ next-crud/src/core/cliente.ts
@@ -0,0 +1,29 @@
+import { NOMEM } from "dns"
+
+export default class Cliente {
+  private #id: string
+  private #nome: string
+  private #idade: number
+
+  constructor(nome: string, idade: number, id: = null ) {
+    this.#nome = nome 
+    this.#idade = idade
+    this.#id = id 
+  }
+
+  static vazio() {
+    return new Cliente('', 0)
+  }
+
+  get id() {
+    return this.#id
+  }
+
+  get nome() {
+    return this.#nome
+  }
+
+  get idade() {
+    return this.#idade
+  }
+}
