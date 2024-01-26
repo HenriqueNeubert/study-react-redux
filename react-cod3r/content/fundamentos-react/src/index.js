e09f14e2161bf44e3f4418e0f376028e63e012b3
HenriqueNeubert <neubert.study@gmail.com>
2024-01-25 21:04:10 -0300

    Feat: first component

--- react-cod3r/content/fundamentos-react/src/index.js
+++ react-cod3r/content/fundamentos-react/src/index.js
@@ -1,10 +1,13 @@
+import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
-import './index.css'
+
+import Primeiro from './components/basics/First'
 
 const title = <h1>Olá!!!</h1>
 ReactDOM.render(
     <div>
+        <Primeiro/>
         { title }
     </div>
     , 
